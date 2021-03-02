2443df3580633d586f6d28a9808160d080b40ffc
MogammadShaqeelless16 <shaqeelless4@gmail.com>
2021-03-02 12:13:56 +0200

    Add project files.

---
+++ ECD-Veri/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ECD_Veri.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
